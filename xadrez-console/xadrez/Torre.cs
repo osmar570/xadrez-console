8ddf0767c9ee202a3860326251fd0dcec109e316
Osmar <osmar570@outlook.com>
2020-04-13 21:38:54 -0300

    Começando a colocar peças no tabuleiro

---
+++ xadrez-console/xadrez/Torre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Cor cor, Tabuleiro tab) : base(cor, tab)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+    
+    
+}
